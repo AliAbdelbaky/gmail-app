7b57cdae7e94bce3b19e5a99a8da1239e9be3a2f
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-10-24 22:14:18 +0200

    init-commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from 'vue';
+
+let count = ref(1)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>
+      Hello from Homepage
+    </h1>
   </main>
 </template>
+<style>
+@media (min-width: 1024px) {
+  main {
+    min-height: 100vh;
+    display: flex;
+    place-items: center;
+  }
+}
+</style>
