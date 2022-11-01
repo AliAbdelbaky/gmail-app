0a280b35df9b826fad9eb09e929e561d786fe83e
Ali Abdelbaky <ali.abdelbaky2000@gmail.com>
2022-11-02 00:37:40 +0200

    app

--- src/plugins/vuetify.ts
+++
@@ -1,9 +0,0 @@
-// Styles
-import "@mdi/font/css/materialdesignicons.css";
-import "vuetify/styles";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import { createVuetify } from "vuetify";
-// Vuetify
-let __components = { components, directives };
-export default createVuetify({...__components});
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-import vuetifyOptions from './vuetify.options'
-module.exports = defineConfig({
-  pluginOptions: {
-    vuetify: {
-      ...vuetifyOptions
-    }
-  },
-});
